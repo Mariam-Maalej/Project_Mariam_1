f42e79b1c6a13481f56e13ee9fa6799109bfb31d
Mariam Maalej <mariam.maalej.gem@gmail.com>
2020-12-30 15:24:55 +0100

    prpare deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connect = require("./config/connect");
+const path = require('path')
 
 //database connection
 connect();
@@ -10,6 +11,14 @@ app.use(express.json());
 app.use("/camp", require("./routes/hike"));
 app.use("/user", require("./routes/user"));
 app.use("/auth", require("./routes/authUser"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV==='production'){
+  app.use(express.static('client/build'))
+  app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 //Port
 const port = process.env.PORT || 5000;
